13973cc6a2566372084978f6f22fe8123aaf83dd
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-12 20:23:36 +0900

    Title: 알파벳에 따른 평가, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240212/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,16 @@
+# 변수 선언, 입력
+c = input()
+
+# 출력
+if c == "S":
+    print("Superior")
+elif c == "A":
+    print("Excellent")
+elif c == "B":
+    print("Good")
+elif c == "C":
+    print("Usually")
+elif c == "D":
+    print("Effort")
+else:
+    print("Failure")
