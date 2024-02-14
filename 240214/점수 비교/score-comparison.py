01d59b918e14fb8dd7969d08e0ad1ed3aa30ec82
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-14 22:34:10 +0900

    Title: 점수 비교, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240214/점수 비교/score-comparison.py
+++ 240214/점수 비교/score-comparison.py
@@ -5,11 +5,8 @@ arrB = input().split()
 mathA = int(arrA[0])
 engA = int(arrA[1])
 
-mathB = int(arrB[1])
+mathB = int(arrB[0])
 engB = int(arrB[1])
 
 # 출력
-if mathA > mathB and engA > engB:
-    print(1)
-else:
-    print(0)
+print(int(mathA > mathB and engA > engB))
