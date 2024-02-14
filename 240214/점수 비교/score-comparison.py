ddb97ee2d94c1e85e7baf058c8050258d358cde3
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-14 22:31:42 +0900

    Title: 점수 비교, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240214/점수 비교/score-comparison.py
@@ -0,0 +1,12 @@
+# 변수 선언, 입력
+arrA = input().split()
+arrB = input().split()
+
+mathA = int(arrA[0])
+engA = int(arrA[1])
+
+mathB = int(arrB[1])
+engB = int(arrB[1])
+
+# 출력
+print(int(mathA > mathB and engA > engB))
