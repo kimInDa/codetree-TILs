180797adbb43285a30d80ee57e143d734fea5ca9
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-15 14:48:21 +0900

    Title: 특정 조건 세 정수 비교, Time: 148ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,16 @@
+# 변수 선언, 입력
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+# 출력
+if a < b and a < c:
+    print(1)
+else:
+    print(0, end = ' ')
+
+if a == b and a == c:
+    print(1)
+else:
+    print(0)
