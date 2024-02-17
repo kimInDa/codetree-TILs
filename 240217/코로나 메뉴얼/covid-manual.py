c0b321f9526819ef08a909e28624dc10d7b1faf0
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-17 22:44:15 +0900

    Title: 코로나 메뉴얼, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240217/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,19 @@
+# 변수 선언, 입력
+a = input().split()
+a_symp = a[0]
+a_temp = int(a[1])
+
+b = input().split()
+b_symp = b[0]
+b_temp = int(b[1])
+
+c = input().split()
+c_symp = c[0]
+c_temp = int(c[1])
+
+
+if a_symp == "Y" and a_temp >= 37:
+    if (b_symp == "Y" and b_temp >= 37) or (c_symp == "Y" and c_temp >= 37):
+        print("E")
+else:
+    print("N")
