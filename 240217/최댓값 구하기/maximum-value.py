32a7853fe2a8b4939ab00f013689517a60be3469
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-17 22:08:20 +0900

    Title: 최댓값 구하기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240217/최댓값 구하기/maximum-value.py
@@ -0,0 +1,13 @@
+# 변수 선언, 입력
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+# 출력
+if a >= b and a >= c:
+    print(a)
+elif b >= a and b >= c:
+    print(b)
+else:
+    print(c)
