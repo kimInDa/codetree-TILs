10edc9ac33c106cc38ac6358fe81311097f38374
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-22 14:36:34 +0900

    Title: 높은 수에서 낮은 수까지, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240222/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,12 @@
+# 변수 선언, 입력
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+# 출력
+if a > b:
+    for i in range(a, b - 1, -1):
+        print(i, end = ' ')
+else:
+    for i in range(b, a - 1, -1):
+        print(i, end = ' ')
