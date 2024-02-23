a141fc587b2266fc531de485dfdcc02ff393e3d0
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-23 22:18:45 +0900

    Title: 배수의 개수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240223/배수의 개수/number-of-multipliers.py
+++ 240223/배수의 개수/number-of-multipliers.py
@@ -1,13 +1,12 @@
 # 변수 선언
-cnt3 = 0
-cnt5 = 0
+cnt3, cnt5 = 0, 0
 
 # 입력, 카운트
 for i in range(10):
     n = int(input())
+    
     if n % 3 == 0:
         cnt3 += 1
-    
     if n % 5 == 0:
         cnt5 += 1
 
