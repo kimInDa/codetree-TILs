17b17a2d38d24e7e30faa88ac91f025080e5a482
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-24 18:15:28 +0900

    Title: 특정 조건 배수의 합, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240224/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -0,0 +1,13 @@
+# 변수 선언, 입력
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+sum_val = 0
+
+# a와 b 사이의 수 중 5의 배수의 합 구하기
+for i in range(a, b + 1):
+    if i % 5 == 0:
+        sum_val += i
+
+# 출력
+print(sum_val)
