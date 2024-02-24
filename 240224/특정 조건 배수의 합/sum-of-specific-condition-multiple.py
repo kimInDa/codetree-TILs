c7a1ecde500f1bb7dbdcbb883a174c5f109ceba4
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-24 18:19:33 +0900

    Title: 특정 조건 배수의 합, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240224/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 240224/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -5,13 +5,17 @@ b = int(arr[1])
 sum_val = 0
 
 # a와 b 사이의 수 중 5의 배수의 합 구하기
-for i in range(a, b + 1):
-    if i % 5 == 0:
-        sum_val += i
+# Case 1. b가 더 큰 경우
+if a <= b:
+    for i in range(a, b + 1):
+        if i % 5 == 0:
+            sum_val += i
 
-for i in range(b, a + 1):
-    if i % 5 == 0:
-        sum_val += i
+# Case 2. a가 더 큰 경우
+else:
+    for i in range(b, a + 1):
+        if i % 5 == 0:
+            sum_val += i
 
 # 출력
 print(sum_val)
