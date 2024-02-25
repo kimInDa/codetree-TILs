0eec1af926bf258b68a5c41e2eec3ea64c7b8049
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-25 18:59:16 +0900

    Title: 완전수 판별기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/완전수 판별기/perfect-number-discriminator.py
+++ 240225/완전수 판별기/perfect-number-discriminator.py
@@ -2,11 +2,12 @@
 n = int(input())
 sum_val = 0
 
+# 1부터 n-1까지의 수중에서 약수의 합 구하기
 for i in range(1, n):
     if i < n and n % i == 0:
         sum_val += i
 
-
+# 출력
 if sum_val == n:
     print("P")
 else:
