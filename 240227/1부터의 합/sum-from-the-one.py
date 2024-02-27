12b5a5272f88707ba7654943abccfd08ea8f1709
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-27 20:38:49 +0900

    Title: 1부터의 합, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/1부터의 합/sum-from-the-one.py
+++ 240227/1부터의 합/sum-from-the-one.py
@@ -2,7 +2,7 @@
 n = int(input())
 sum_val = 0
 
-# 1부터 n까지 차례대로 더한 값이 n이상이 되는 순간 합한 값을 출력
+# 1부터 n까지 차례대로 더한 값이 n이상이 되는 순간 더한 값을 출력
 for i in range(1, n + 1):
     sum_val += i
     if sum_val >= n:
