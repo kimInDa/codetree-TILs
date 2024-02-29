1f0200b07055555804c3e71032d2e5abcfb8f430
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-29 14:11:32 +0900

    Title: 20대의 평균나이, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240229/20대의 평균나이/average-age-of-20.py
+++ 240229/20대의 평균나이/average-age-of-20.py
@@ -6,10 +6,14 @@ while True:
     # 변수 선언, 입력
     n = int(input())
 
+    # 나이가 20대가 아닌 경우 loop 종료
     if n > 29 or n < 20:
         break
+
+    # 나이가 20대인 경우 나이의 총합과 인원수 구하기
     else:
         sum_val += n
         cnt += 1
 
+# 20대인 사람의 평균 출력
 print(f"{sum_val / cnt:.2f}")
