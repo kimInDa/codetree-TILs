fc473b8abc0c1412c79a22e4977aac42d84497c2
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-29 14:08:10 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240229/20대의 평균나이/average-age-of-20.py
@@ -0,0 +1,15 @@
+# 20대인 사람의 평균 구하기
+sum_val = 0
+cnt = 0
+
+while True:
+    # 변수 선언, 입력
+    n = int(input())
+
+    if n // 10 > 2:
+        break
+    else:
+        sum_val += n
+        cnt += 1
+
+print(f"{sum_val / cnt:.2f}")
