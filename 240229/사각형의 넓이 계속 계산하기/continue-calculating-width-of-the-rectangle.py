0e0629f379dbb5e2558e6fcdc9c2c6dd95c806d2
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-02-29 13:50:46 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240229/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.py
+++ 240229/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.py
@@ -1,11 +1,14 @@
 # 사각형의 넓이를 문자 C가 나올 때까지 출력
 while True:
+    # 변수 선언, 입력
     arr = input().split()
     w = int(arr[0])
     h = int(arr[1])
     c = arr[2]
 
+    # 사각형의 넓이 출력
     print(w * h)
 
+    # 문자 C가 나오면 loop 종료
     if c == "C":
         break
