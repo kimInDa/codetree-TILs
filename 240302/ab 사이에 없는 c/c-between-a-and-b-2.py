1207506cd41b577a6cd2e5a870c4fff777609da7
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-03-02 14:11:26 +0900

    Title: ab 사이에 없는 c, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240302/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240302/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -8,8 +8,8 @@ satisfied = True
 
 # a이상 b이하에 c의 배수가 없는지 판별
 for i in range(a, b + 1):
-    if i % c != 0:
-        satisfied = True
+    if i % c == 0:
+        satisfied = False
 
 # c의 배수가 없다면 "YES", 있다면 "NO"를 출력
 if satisfied == True:
