2cb1e1511f73b48f10f8409bbdd375f9a23af383
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-03-13 23:14:10 +0900

    Title: 좌우로 반복하며 출력, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -0,0 +1,25 @@
+# 변수 선언, 입력
+n = int(input())
+
+# n에 따라 일의 숫자를 좌우로 반복하며 출력
+for i in range(1, n + 1):
+    # 홀수행일 경우 1부터 n까지 출력
+    if i % 2 != 0:
+        for j in range(1, n + 1):
+            print(j, end = "")
+    # 짝수행일 경우 n부터 1까지 출력
+    else:
+        for j in range(n, 0, -1):
+            print(j, end = "")
+
+    # 각 행이 끝날 때마다 줄바꿈
+    print()
+
+# 해설 풀이법
+for i in range(n):
+	for j in range(n):
+		if i % 2 == 0:
+			print(j + 1, end="")
+		else:
+			print(n - j, end="")
+	print()
