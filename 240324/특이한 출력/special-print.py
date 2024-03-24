5893115b70ecea2acda3b172e6627b7bc3f9b649
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-03-24 19:07:51 +0900

    Title: 특이한 출력, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240324/특이한 출력/special-print.py
+++ 240324/특이한 출력/special-print.py
@@ -2,10 +2,10 @@
 n = int(input())
 
 # (1, 1)에서 (n, n)까지 출력
-# (i, j)에서 i + j가 4의 배수가 되는 순간 다음 줄로 넘어감
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         print(f"({i}, {j})", end = " ")
 
+        # (i, j)에서 i + j가 4의 배수가 되는 순간 다음 줄로 넘어감
         if (i + j) % 4 == 0:
             print()
