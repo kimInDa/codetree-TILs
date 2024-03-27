f1f94e25c225678220086254fa14278cb62d69ee
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-03-27 15:05:37 +0900

    Title: 정사각 알파벳 출력하기, Time: 112ms, Memory: 73MB, Status: Wrong Answer - CodeTree

--- 240327/정사각 알파벳 출력하기/print-alpabet-in-square.py
+++ 240327/정사각 알파벳 출력하기/print-alpabet-in-square.py
@@ -1,9 +1,11 @@
 # 변수 선언, 입력
 n = int(input())
+cnt = 65
 
 # n x n 크기의 알파벳 정사각형 출력
 for i in range(n):
-    for j in range(65, 65 + n):
-        print(chr(j), end = "")
+    for j in range(n):
+        print(chr(cnt), end = "")
+        ++cnt
 
     print()
