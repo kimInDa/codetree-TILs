69f381533b353bdf166aa6fcf6040905bbe9403a
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-03-28 15:37:49 +0900

    Title: 알파벳 출력하기, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 240328/알파벳 출력하기/print-alphabet.py
+++ 240328/알파벳 출력하기/print-alphabet.py
@@ -4,9 +4,9 @@ cnt = 'A'
 
 # 아스키 코드를 이용한 알파벳 출력
 for i in range(n):
-    for j in range(i + 1):
+    for _ in range(i + 1):
         print(cnt, end = "")
         cnt = chr(ord(cnt) + 1)
-        if ord(cnt) == 91:
+        if ord(cnt) > ord("Z"):
             cnt = "A"
     print()
