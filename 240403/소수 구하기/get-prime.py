cb2956c949b08f7168fd535b4d0ed5d35f3ace03
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-04-03 16:38:52 +0900

    Title: 소수 구하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/소수 구하기/get-prime.py
+++ 240403/소수 구하기/get-prime.py
@@ -3,6 +3,9 @@ n = int(input())
 
 # 1이상 n이하의 수 중 소수를 오름차순으로 출력
 for i in range(2, n + 1):
-    for j in range(2, n + 1):
-        if i != j and i % j != 0:
-            print(i, end = " ")
+    for j in range(2, i + 1):
+        if i == j:
+            print(i, end = ' ')
+
+        if i % j == 0:
+           break;
