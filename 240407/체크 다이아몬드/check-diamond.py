16830da50894d2d4b1bed031eccaa85f91d0fc52
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-04-07 18:20:42 +0900

    Title: 체크 다이아몬드, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240407/체크 다이아몬드/check-diamond.py
+++ 240407/체크 다이아몬드/check-diamond.py
@@ -8,3 +8,24 @@ for i in range(1, n+1):
 # 하단 부분 그리기
 for i in range(n-1, 0, -1):
     print(" " * (n-i) + "* " * i)
+
+
+# 풀이
+# 변수 선언 및 입력
+n = int(input())
+
+# 모양에 맞게 위쪽 별을 출력합니다.
+for i in range(n):
+	for _ in range(n - i - 1):
+		print(" ", end="")
+	for _ in range(i + 1):
+		print("* ", end="")
+	print()
+
+# 모양에 맞게 아래쪽 별을 출력합니다.
+for i in range(n-2, -1, -1):
+	for _ in range(n - i - 1):
+		print(" ", end="")
+	for _ in range(i + 1):
+		print("* ", end="")
+	print()
