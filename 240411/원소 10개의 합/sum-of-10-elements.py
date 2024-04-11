0279c0809de8b9a35cd64f1e8a7cb6a2fada0ce7
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-04-11 20:18:27 +0900

    Title: 원소 10개의 합, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240411/원소 10개의 합/sum-of-10-elements.py
+++ 240411/원소 10개의 합/sum-of-10-elements.py
@@ -8,3 +8,15 @@ for i in range(10):
 
 # 출력
 print(sum_val)
+
+# # 풀이 2. for in 이용
+# # 입력 받은 수를 배열에 숫자로 변환하여 저장
+# arr = list(map(int, input().split()))
+# sum_val = 0
+
+# # 입력 받은 수를 더하기
+# for elem in arr:
+#     sum_val += elem
+
+# # 합 출력
+# print(sum_val)
