e8d5de016571e7f414d99be30c2af330c81cef69
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-04-19 23:33:07 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
+++ 240419/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -7,12 +7,11 @@ sum_val = 0;
 
 # 0 이전까지의 수 중 2의 배수 갯수와 합계 구하기
 for elem in arr:
-    if elem != 0:
-        if elem % 2 == 0:
-            cnt += 1
-            sum_val += elem
-    else:
+    if elem == 0:
         break;
+    if elem % 2 == 0:
+        cnt += 1
+        sum_val += elem
 
 # 출력
 print(cnt, sum_val)
