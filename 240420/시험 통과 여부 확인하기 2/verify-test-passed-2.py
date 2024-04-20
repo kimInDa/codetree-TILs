f7056a6c081ae112c2891738cd0bf43d7c3f9623
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-04-20 16:29:12 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240420/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 240420/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -1,8 +1,10 @@
-# 변수 선언, 입력
+# 학생 수 입력 받기
 n = int(input())
+
+# 통과한 학생 수를 나타내는 변수
 cnt = 0;
 
-# 결과 출력
+# 학생별 통과 여부 출력
 for i in range(n):
     # 점수 입력
     scores_arr = list(map(int,input().split())) 
@@ -12,8 +14,10 @@ for i in range(n):
     for elem in scores_arr:
         sum_val += elem
 
-    # 평균에 따른 결과 출력
+    # 평균 구하기
     avg_val = sum_val / len(scores_arr)
+    
+    # 평균에 따른 결과 출력
     if(avg_val >= 60):
         print("pass")
         cnt += 1
