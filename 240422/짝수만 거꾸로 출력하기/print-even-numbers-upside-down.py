08413a8e69af96a836ae9fdec40a3d9de4f554f1
kimInDa <105577805+kimInDa@users.noreply.github.com>
2024-04-22 15:12:28 +0900

    Title: 짝수만 거꾸로 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240422/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
+++ 240422/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -7,17 +7,17 @@ even_arr = [];
 # n개의 정수 입력 받기
 for _ in range(n):
     num = int(input())
-    
+
     # 입력된 수가 짝수이면 배열에 저장
     if(num!=0 and num % 2 == 0):
         even_arr.append(num)
 
-# 배열의 길이
-length = len(even_arr)
+# 배열 뒤집기
+reversed_even_arr = even_arr[::-1]
 
 # 짝수 배열 역순으로 출력하기
-if length == 0:
-    print(length)
+if len(reversed_even_arr) == 0:
+    print(0)
 else :
-    for i in range(length, 0, -1):
-        print(i, end = " ")
+    for elem in reversed_even_arr:
+        print(elem, end = " ")
